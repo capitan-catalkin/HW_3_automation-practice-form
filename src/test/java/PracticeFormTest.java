7296d0bfd806939fbeecef3c5f891ea82c2ec673
urrrrk <pomogitemnenaiti@mail.ru>
2023-09-14 21:21:21 +0300

    исправления замечаний

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -5,45 +5,42 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 
-public class TestPracticeForm {
+public class PracticeFormTest {
     @BeforeAll
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";  //тест не дожидается полной загрузки страницы а начинает проходится как появляются нужные элементы
     }
 
     @Test
     void fillFormTest(){
-        Configuration.pageLoadStrategy = "eager";  //тест не дожидается полной загрузки страницы а начинает проходится как появляются нужные элементы
-        //executeJavaScript("$('#fixedban').remove()");
-        //executeJavaScript("$('footer').remove()");   //убирает банеры со страницы которые мешают нажатию кнопок
-
-
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");   //убирает банеры со страницы которые мешают нажатию кнопок
 
         $("#firstName").setValue("Sergey");
         $("#lastName").setValue("Esenin");
         $("#userEmail").setValue("esenin@mail.ru");
-        $(".custom-control-label").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1900");
         $(".react-datepicker__day--021").click();
-        //вопрос к проверяющим$(".subjects-auto-complete__value-container--is-multi").click();
-        // почему можно кликнуть на это поле по этому локатору, но нельзя что то ввести?
         $("#subjectsInput").setValue("b").pressEnter();
-        $("label[for='hobbies-checkbox-2']").click();  //выбор чекбокса по названию (сначала выбрал эдемент label и потом название for
-        $(".form-control-file").uploadFile(new File("src/test/resources/cat1.jpg"));
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("cat1.jpg");;
         $("#currentAddress").setValue("Konstantinovo");
-        $("#state").click();
-        $("#react-select-3-option-1").click();
+        $("#state").click(); //выбрал поле где надо искать совпадение
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click(); //выбрал элемент по id и начал искать совпадения
         $("#city").click();
-        $("#react-select-4-option-1").click();
+        $("#stateCity-wrapper").$(byText("Lucknow")).click();;
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
