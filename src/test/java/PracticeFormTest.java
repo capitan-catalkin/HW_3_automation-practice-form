fdb7bc154e5ece57bb57d20a0996099985adefcd
ayr <vj_1@list.ru>
2023-11-07 15:01:39 +0300

    Jenkins

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -3,7 +3,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
